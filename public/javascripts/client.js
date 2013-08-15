a52d6e0a2cd5daf2d3b5133458818aec0a6105b6
taewony <engchat@gmail.com>
2013-08-15 20:21:41 +0900

    initial commit for LastBrownie App.

---
+++ public/javascripts/client.js
@@ -0,0 +1,276 @@
+var SECOND = 1000,
+    MINUTE = SECOND * 60,
+    HOUR   = MINUTE * 60,
+    DAY    = HOUR   * 24,
+    MONTH  = DAY    * 31;
+
+if(!$.fn.hoverBio){
+  $.fn.hoverBio = function(target){
+    var thisObj = $(this[0]),
+        targetObj = (target||thisObj).children('.bio');
+    return thisObj.hover(
+      function(e){
+        targetObj.css('left',e.pageX+10).css('top',e.pageY).show();
+      },function(){
+        targetObj.hide();
+      } 
+    );
+  };
+}
+
+if(!$.fn.hoverPic){
+  $.fn.hoverPic = function(){
+    return  $(this[0]).find('a').each(function(){
+      var thisObj = $(this),
+          thisImg = thisObj.children('img.twitpic');
+      if(thisImg){
+        thisObj.hover(
+          function(e){
+            thisImg.css('left',e.pageX+10).css('top',e.pageY).show();
+          },function(){
+            thisImg.hide();
+          } 
+        );
+      }
+    });
+  };
+}
+
+if(!$.fn.ago){
+  $.fn.ago = function(){
+    for(var i=this.length; i--;){
+      var thisObj = $(this[i]),
+          timestamp = Date.parse(thisObj.attr('datetime')),
+          duration = new Date() - timestamp;
+      if(duration<MINUTE){
+        thisObj.text((duration/SECOND).toPrecision(2)+' secconds ago');
+      }else if(duration<HOUR){
+        thisObj.attr('class','min').text((duration/MINUTE).toPrecision(2)+' minutes ago');
+      }else if(duration<DAY){
+        thisObj.attr('class','hr').text((duration/HOUR).toPrecision(2)+' hours ago');
+      }else if(duration<MONTH){
+        thisObj.attr('class','day').text((duration/DAY).toPrecision(2)+' days ago');
+      }else{ 
+        thisObj.attr('class','').text(new Date(timestamp).toLocaleString().replace(/GMT.+/,''));
+      }
+    }
+    return this;
+  };
+}
+
+$(function(){
+  var socket = io.connect('http://127.0.0.1:3000');
+  var windowObj = $(window),
+      documentObj = $(document),
+      page = 1,
+      reply_id = '',
+      selected_id = 'all',
+      chatObj = $('#chat'),
+      name = $('#name').text(),
+      textObj = $('#text').focus(),
+      sidebarObj = $('#sidebar'),
+      allObj = $('#all'),
+      oldestId = new Array(50); // the length should be larger than the digits of message.id_str==the newest id as initial value
+  var increaseBadge = function(j,v){return ++v;};
+  var decreaseBadge = function(j,v){return --v;};
+  var deleteOutDiv = function(id_str){
+    var jQid = '#'+id_str,
+        idObj = $(jQid);
+    if(idObj.length){
+      $('#'+idObj.attr('class')+'.sidebar a span.badge').text(decreaseBadge);
+      $('#all.sidebar a span.badge').text(decreaseBadge);
+      if(idObj.hasClass('mentions')){
+        $('#mentions.sidebar a span.badge').text(decreaseBadge);
+      }
+    }
+    idObj.before($(jQid+'>div').css('margin-left','0px'));
+    idObj.remove();
+  };
+  socket.on('message', function(message){
+    if(message.count){
+      $('#count').text(message.count);
+    }else if(message['delete']){
+      deleteOutDiv(message['delete'].status.id_str);
+    }else{
+      var messageArray = Array.isArray(message) ? message.reverse() : [message];
+      for(var k=messageArray.length; k--;){
+        var message=messageArray[k];
+        if(message.text){
+          var id = message.id_str,
+              scroll = false,
+              jQid = '#'+id,
+              idObj = $(jQid);
+          if((id.length===oldestId.length && id<oldestId) || id.length<oldestId.length){
+            oldestId = id;
+            scroll = true;
+          }
+
+          if(scroll || !(idObj.length)){
+            var entities = message.entities,
+                urls = entities.urls,
+                user_mentions = entities.user_mentions,
+                text = message.text,
+                mentioned = false,
+                user = message.user,
+                uid = user.id_str,
+                jQuid = '#'+uid,
+                screen_name = user.screen_name,
+                profile_image_url = user.profile_image_url,
+                in_reply_to_status_id_str = message.in_reply_to_status_id_str,
+                uidObj = $(jQuid);
+            // TODO retweets should be merged with the original
+            for(var i=urls.length, j=user_mentions.length; (i--)+(j--);){
+              if(j<0 || (i>=0 && urls[i].indices[0]>user_mentions[j].indices[0])){// check j and i first
+                var href = urls[i].expanded_url,
+                    src = href.match(/http:\/\/twitpic\.com\/(\w+)/) ? 'http://twitpic.com/show/thumb/'+RegExp.$1 :
+                          href.match(/http:\/\/yfrog\.com\/(\w+)/) ? 'http://yfrog.com/'+RegExp.$1+':small' :
+                          href.match(/http:\/\/instagr\.am\/p\/(\w+)/) ? 'http://instagr.am/p/'+RegExp.$1+'/media/?size=t' :
+                          href.match(/http:\/\/i\.imgur\.com\/(\w+)\.jpg/) ? 'http://i.imgur.com/'+RegExp.$1+'s.jpg' :
+                          href.match(/http:\/\/plixi\.com\/p\//) ? 'http://api.plixi.com/api/tpapi.svc/imagefromurl?size=thumbnail&url='+href : null,
+                    img = src ? ' style="font-weight:bold">'+href+'<img src="'+src+'" class="twitpic"/>' : '>'+href;
+                text = text.slice(0, urls[i].indices[0])+'<a href="'+href+'" target="_blank"'+img+'</a>'+text.slice(urls[i].indices[1]);
+                ++j;// put back the other one
+              }else{
+                mentioned = (user_mentions[j].screen_name === name);// true or false
+                text = text.slice(0, user_mentions[j].indices[0]) +
+                        '@<a href="#" onclick="select(\''+user_mentions[j].id+'\');" style="text-decoration:none;color:#2e991b">' + 
+                        user_mentions[j].screen_name + '</a>' + text.slice(user_mentions[j].indices[1]);
+                ++i;// put back the other one
+              }
+            }
+            var p_str = '<p><a href="#" onclick="select(\''+uid+'\');"><img src="'+profile_image_url+'" class="profile"/></a> '+text+'</p><span class="permalink"><span><time class="sec" datetime="'+message.created_at+'"></time> from</span> '+message.source+' | </span>',
+                rp_str = '<a onclick="reply(\''+id+'\',\''+screen_name+'\');" href="#">Reply</a>';
+            if(screen_name===name){
+              p_str += '<span class="permalink">'+rp_str+' - <a onclick="destroy(\''+id+'\');" href="#">Delete</a></span>';
+            }else{
+              p_str += '<span class="permalink"><a onclick="retweet(\''+id+'\');" href="#">Retweet</a> - '+rp_str+'</span>';
+            }
+            if(idObj.length){
+              idObj.html(p_str).addClass(uid);
+            }else{
+              var d_str = '<div id='+id+' class="'+uid+'">'+p_str+'</div>';
+              if(scroll){
+                chatObj.append(d_str);
+              }else{
+                chatObj.prepend(d_str);
+              }
+            }
+            $(jQid).hoverPic();
+            if(in_reply_to_status_id_str){
+              if(scroll){
+                $(jQid).append('<div id='+in_reply_to_status_id_str+' style="margin-left: 14px;"></div>');
+              }else{
+                $(jQid).append($('#'+in_reply_to_status_id_str).css('margin-left', '14px'));
+              }
+            }
+            if(!(uidObj.length)){
+              var bioObj = $('<div id='+uid+' class="sidebar" onclick="select(\''+uid+'\');"><a href="#"><img src="'+profile_image_url+'" /> (<span class="badge">0</span>) '+screen_name+'</a><div class="bio"><img src="'+profile_image_url+'" /><span><b class="fullname">'+user.name+'</b><br/><span>@'+screen_name+'</span><br/>'+user.location+'<br/><b>Web:</b> <a href="'+user.url+'" target="_blank">'+user.url+'</a><br/><b>Bio:</b> '+user.description+'</span></div></div>').hoverBio();
+              if(scroll){
+                sidebarObj.append(bioObj);
+              }else{
+                allObj.after(bioObj);
+              }
+              uidObj = $(jQuid);
+            }else if(!scroll){
+              allObj.after(uidObj);
+            }
+            $(jQuid+'.sidebar a span.badge').text(increaseBadge);
+            $('#all.sidebar a span.badge').text(increaseBadge);
+            if(mentioned){
+              $('#mentions.sidebar a span.badge').text(increaseBadge);
+              $(jQid).addClass('mentions');
+            }
+            if(uid!==selected_id && selected_id !== 'all'){
+              $(jQid+' :not(:has(.'+selected_id+'))').parentsUntil('#chat').hide();
+            }
+            $(jQid+' p a img.profile').hoverBio(uidObj);
+            $(jQid).data('user_mentions', user_mentions);
+          }
+        }else{
+          console.log(message);
+        }
+      }
+    }
+  });
+
+  setInterval(function(){ $('time.sec').ago(); }, SECOND);
+  setInterval(function(){ $('time.min').ago(); }, MINUTE/6);
+  setInterval(function(){ $('time.hr').ago(); }, HOUR/4);
+  setInterval(function(){ $('time.day').ago(); }, DAY/3);
+
+  textObj.keydown(function(e){
+    if((e.keyCode||e.which)===13){ // return key
+      document.send();
+      e.preventDefault();
+    }
+  });
+  textObj.keyup(function(e){
+    $('#countChar').text(140-$(this).text().length);
+    if(textObj.text()===''){
+      reply_id = '';
+    }
+  });
+
+  this.retweet = function(id_str){
+    socket.emit('retweet', {id_str: id_str});
+    textObj.focus();
+    return false;
+  };
+
+  this.reply = function(id_str, screen_name){
+    var rms = ['@'+screen_name, ''],
+        ums = $('#'+id_str).data('user_mentions');
+    for(var i=ums.length; i--;){
+      if(ums[i].screen_name !== screen_name && ums[i].screen_name !== name){
+        rms.splice(-1, 0, '@'+ums[i].screen_name);
+      }
+    }
+    textObj.text(rms.join(' '));
+
+    textObj.focus();
+    reply_id = id_str;
+    selected_id = 'reply';
+    $('#chat> :not(:has(#'+id_str+'), #'+id_str+')').hide();
+    return false;
+  };
+  
+  this.destroy = function(id_str){
+    deleteOutDiv(id_str);
+    socket.emit('destroy', {id_str: id_str});
+    textObj.focus();
+    return false;
+  };
+
+  this.send = function(){
+    var text = textObj.text();
+    
+    if(text && name){
+      socket.emit('update', {text: text, in_reply_to_status_id: reply_id, include_entities: true});
+      textObj.text('');
+      reply_id = '';
+      textObj.focus();
+      this.select('all');
+    }else{
+      alert('Oops, blank message...');
+    }
+    return false;
+  };
+
+  this.select = function(id_str){
+    selected_id = id_str;
+    $('#'+id_str+' .bio').hide();
+    $('.sidebar').css('background', '#fff');
+    $('#'+id_str).css('background', '#ddd');
+    $('#chat').find(':hidden:not(.twitpic)').show();
+    if(id_str!=='all'){
+      $('#chat> :not(:has(.'+id_str+'), .'+id_str+')').hide();
+    }
+    return false;
+  };
+
+  windowObj.scroll(function(){
+    if (documentObj.height() - windowObj.height() - windowObj.scrollTop() <= 0){
+      socket.emit('scroll', {page: ++page, include_entities: true});
+    }
+  });
+});
---
+++ web.js
@@ -0,0 +1,283 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , RedisStore = require('connect-redis')(express)
+  , Twitter = require('./twitter')
+  , connect = require('connect')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , socket = require('socket.io')
+  , redis = require('redis')
+  , app = express();
+  
+var cookieParser = express.cookieParser('your secret sauce')
+  , sessionStore = new connect.middleware.session.MemoryStore();
+var consumerKey = 'ACc6vs7VOl39LaGTym4ybw',
+    consumerSecret = 'LnC3eUkYCpLXEqJZalKidKHBFQthG7WbmHWT0dTWY';
+    
+app.configure(function () {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(cookieParser);
+  app.use(express.session({ store: sessionStore }));
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+// development only
+app.configure('development', function(){
+  express.logger('development node');
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  express.logger('production node');
+  app.use(express.errorHandler()); 
+});
+
+var server =  app.listen(app.get('port')) // http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+var SessionSockets = require('session.socket.io')
+  , sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
+
+// Routes
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+app.get('/home', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/game', function(req, res){
+  jadeFile = 'game.jade';
+  loginMessage = 'Home';
+  loginTo = '/logout';
+  var screenName = 'Anyone';
+  if (req.session.oauth) {
+    try{
+      screenName = req.session.oauth._results.screen_name; // undefined error
+      console.log("screanName = " + screenName);
+    }catch(e){
+      console.error('screen_name ERROR: ' + e);
+      setTimeout(res.redirect, 500, '/login');
+    }
+  }
+  
+  res.render(jadeFile, {
+    title: 'LastBrownie',
+    loginm: loginMessage,
+    loginto: loginTo,
+    screen_name: screenName
+  });
+});
+
+app.get('/login', function(req, res){
+  var tw = new Twitter(consumerKey, consumerSecret);
+  tw.getRequestToken(function(error, url){
+    if(error){
+      req.session.destroy(function(){
+        console.error(error);
+        res.writeHead(500, {'Content-Type': 'text/html'});
+        res.send('ERROR :' + error);
+      });
+    }else{
+      req.session.oauth = tw;
+      res.redirect(url);
+    }
+  });
+});
+
+app.get('/logout', function(req, res){
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
+});
+
+// authorized callback from twitter.com
+app.get('/authorized', function(req, res, next){
+  if (req.session.oauth) {
+    var tw = new Twitter(consumerKey, consumerSecret, req.session.oauth);
+    tw.getAccessToken(req.query.oauth_verifier, function(error){
+      if(error){
+        req.session.destroy(function(){
+          console.error(error);
+          res.send(error);
+        });
+      }else{
+        req.session.oauth = tw;
+        console.log('user_id = ' + tw._results.user_id);
+        res.redirect('/game');
+      }
+    });
+  }
+  else {
+    res.redirect('/login'); // Redirect to login page
+  }
+});
+
+io.sockets.tid2clt = {};
+io.sockets.broadcastTo = function(to, message){ //to has to be an Array
+  try{
+    for(var i=to.length; i--;){
+      var clt = this.tid2clt[to[i]];
+      if(clt){
+        if(this.flags.json){
+          clt.json.send(message);
+        }else{
+          clt.send(message);
+        }
+      }
+    }
+  }catch(e){
+    console.error('broadcastTo ERROR: '+e);
+  }
+  return this;
+};
+
+var count = 0,
+    maxcount = 0;
+    
+
+sessionSockets.on('connection', function (err, socket, session) {
+  count++;
+  console.log('count = '+count);
+  socket.json.broadcast.send({count: count});
+  socket.json.send({count: count});
+  if(count>maxcount){
+    console.log('maxcount: '+(maxcount=count));
+  }
+  
+  var sessionID = socket.id;
+  console.log('A socket with sessionID '+sessionID+' connected!');
+  // setup an inteval that will keep our session fresh
+
+  if(session.oauth){
+    session.save(); // ???
+    var tw = new Twitter(consumerKey, consumerSecret, session.oauth);
+    try{
+      io.sockets.tid2clt[tw._results.user_id] = socket;
+    }catch(e){
+      console.error('io.sockets.tid2sid ERROR: ' + e);
+    }
+    
+    //view home timeline
+    var scroll = function(params){
+      tw.getTimeline(params, function(error, data, response){
+        if(error){
+          console.error('TIMELLINE ERROR: ' + error);
+        }else{
+          socket.json.send(data);
+          //req.session.page.push(data);
+        }
+      });
+    };
+    scroll({page: 1, include_entities: true});
+    //manage followers
+    if(!socket.followers){
+      tw.followers(function(error, data, response){
+        if(error){
+          console.error('FOLLOWERS ERROR: ' + error);
+        }else{
+          socket.followers = data;
+        }
+      });
+    }
+    
+    //user streams
+    var usParams = {include_entities: true},
+        stream = tw.openUserStream(usParams);
+    stream.on('data', function(data){
+      try{
+        if(data.friends){
+        }else{
+          socket.json.send(data);
+        }
+      }catch(e){
+        console.error('dispatch event ERROR: ' + e);
+      }
+    });
+    
+    stream.on('error', function(err){
+      session.destroy(function(){
+        console.error('UserStream ERROR: ' + err);
+      });
+    });
+    
+    stream.on('end', function(){
+      session.destroy(function(){
+        console.log('UserStream ends successfully');
+      });
+    });
+  }
+
+  socket.on('update', function(message){
+    tw.update(message, function(error, data, response){
+      if(error){
+        console.error("UPDATE ERROR\ndata: "+data+'response: '+response+'oauth: '+tw+'message: '+message);
+      }else{
+        socket.json.send(data);
+        io.sockets.json.broadcastTo(socket.followers, data);
+      }
+    });
+  });
+  
+  socket.on('retweet', function(message){
+    tw.retweet(message.id_str, function(error, data, response){
+      if(error){
+        console.error("RETWEET ERROR\ndata: "+data+'response: '+response+'oauth: '+tw+'message: '+message);
+      }else{
+        socket.json.send(data);
+        io.sockets.json.broadcastTo(socket.followers, data);
+      }
+    });
+  });
+  
+  socket.on('destroy', function(message){
+    tw.destroy(message.id_str, function(error, data, response){
+      if(error){
+        console.error("DELETE ERROR\ndata: "+data+'response: '+response+'oauth: '+tw+'message: '+message);
+      }
+    });
+  });
+  
+  socket.on('scroll', function(message){
+    scroll(message);
+  });
+  
+  socket.on('disconnect', function(){
+    count--;
+    socket.json.broadcast.send({count: count});
+  });
+  
+  // Based on http://www.danielbaulig.de/socket-ioexpress/
+  var intervalID = setInterval(function(){
+      // reload the session (just in case something changed,
+      // we don't want to override anything, but the age)
+      // reloading will also ensure we keep an up2date copy
+      // of the session with our connection.
+      session.reload(function(){ 
+          // "touch" it (resetting maxAge and lastAccess)
+          // and save it back again.
+          session.touch().save();
+      });
+  }, 60*1000);
+  
+  socket.on('disconnect', function(){
+    console.log('A socket with sessionID '+sessionID+' disconnected!');
+    // clear the socket interval to stop refreshing the session
+    clearInterval(intervalID);
+  });
+});
+
+server.listen(app.get('port'));
+console.log('Express server listening on port ' + app.get('port'));
